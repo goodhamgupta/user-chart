b1615829124712487c3c8e9baa7f2252716f31e4
goodhamgupta <shubhamg2208@live.com>
2016-08-08 18:13:54 +0530

    Function to insert data added.

--- graphs/charts/urls.py
+++ graphs/charts/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^view/', views.chart),
-    url(r'^data/([1-5])$',views.get_data)
+    url(r'^data/([1-5])$',views.get_data),
+    url(r'^insert/',views.insert_data)
 ]
--- graphs/charts/views.py
+++ graphs/charts/views.py
@@ -5,13 +5,55 @@ from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework import viewsets
 import requests
 from models import CountryModel
-
+from .vars import population,population_density,life_expectancy,elevation,surface_area
 
 # app_id = "5f68be87"
 # app_key = "a5699ef989e61a480601bc60f022f559"
 # def get_data(request):
 #     r=requests.get("http://api.undata-api.org/organizations?app_id={"+app_id+"}&app_key={"+app_key+"}")
 #     print r.content
+
+@api_view(['GET'])
+def insert_data(request):
+
+    for obj in population:
+        country = obj['country']
+        population = obj['population']
+        b = CountryModel(country=country,population=population)
+        b.save()
+    for obj in life_expectancy:
+        try:
+            b = CountryModel.objects.get(country=obj['country'])
+            b.life_expectancy = obj['expectancy']
+            b.save()
+        except Exception as e:
+            continue
+
+    for obj in surface_area:
+            try:
+                b = CountryModel.objects.get(country=obj['country'])
+                b.area = obj['area']
+                b.save()
+            except:
+                continue
+
+    for obj in population_density:
+            try:
+                b = CountryModel.objects.get(country=obj['country'])
+                b.density = obj['density']
+                b.save()
+            except Exception as e:
+                continue
+
+    for obj in elevation:
+            try:
+                b = CountryModel.objects.get(country=obj['country'])
+                den = obj['average'][:-1]
+                b.elevation = den
+                b.save()
+            except Exception as e:
+                continue
+    return Response("Complete")
 
 def chart(request):
     #return Response({"extra":"extra"},template_name="/static/charts/base.html")
