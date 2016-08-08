a1d364f6e6870c5aa4350f6ad2d1fca5773177c7
Shubham Gupta <shubhamg2208@live.com>
2016-08-08 07:40:47 +0530

    Minor changes.

--- graphs/charts/views.py
+++ graphs/charts/views.py
@@ -3,10 +3,16 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework import viewsets
+import requests
 
+app_id = "5f68be87"
+app_key = "a5699ef989e61a480601bc60f022f559"
+def get_data(request):
+    r=requests.get("http://api.undata-api.org/organizations?app_id={"+app_id+"}&app_key={"+app_key+"}")
+    print r.content
 
 def chart(request):
     print "Rendering"
     #return Response({"extra":"extra"},template_name="/static/charts/test.html")
-    return render(request,"static/charts/test.html")
+    return render(request,"charts/test.html")
 
--- graphs/graphs/settings.py
+++ graphs/graphs/settings.py
@@ -69,6 +69,8 @@ TEMPLATES = [
     },
 ]
 
+
+
 WSGI_APPLICATION = 'graphs.wsgi.application'
 
 
@@ -124,10 +126,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
+
+TEMPLATE_DIRS = (
+    PROJECT_ROOT + '/templates/',
+)
