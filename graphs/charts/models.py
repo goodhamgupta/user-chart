f50001afdec2690dd9ff9409cdfa1e289c0ffc02
goodhamgupta <shubhamg2208@live.com>
2016-08-08 12:30:08 +0530

    Bootstrap fixed. Bar graph/scatter graph working!

--- graphs/charts/models.py
+++ graphs/charts/models.py
@@ -8,5 +8,6 @@ from django.db import models
 class CountryModel(models.Model):
     country = models.CharField(null=False,max_length=200)
     population = models.CharField(max_length=1000)
+    life_expectancy = models.CharField(max_length=1000)
     density = models.CharField(max_length=1000)
     literacy = models.CharField(max_length=1000)
